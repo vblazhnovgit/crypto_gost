871bf249ec46c21a7c916970f366949321ed6c89
vblazhnov <vblazhnov@yandex.ru>
2019-04-22 12:35:26 +0300

    Updated README and spec test

--- spec/crypto_gost3410/crypto_gost3410_spec.rb
+++ spec/crypto_gost3410/crypto_gost3410_spec.rb
@@ -23,16 +23,15 @@ describe CryptoGost3410 do
         let(:public_key) { group.generate_public_key private_key }
         let(:message) { Faker::Lorem.sentence(3) }
         let(:size) { if name.start_with?('Gost512') then 512 else 256 end }
-        let(:unpack_format) { if name.start_with?('Gost512') then 'Q*' else 'L*' end}
         let(:digest) { CryptoGost3411::Gost3411.new(size/8).update(message).final }
-        let(:digest_num) { digest.unpack(unpack_format)[0] }
+        let(:digest_num) { digest.reverse.unpack('H*')[0].hex }
         let(:generator) { CryptoGost3410::Generator.new(group) }
         let(:rand_val) { SecureRandom.random_number(1..group.order-1) }
         let(:signature) { generator.sign(digest_num, private_key, rand_val) }
         let(:verifier) { CryptoGost3410::Verifier.new(group) }
         let(:another_message) { Faker::Lorem.sentence(2) }
         let(:another_digest) { CryptoGost3411::Gost3411.new(size/8).update(another_message).final}
-        let(:another_digest_num) { another_digest.unpack(unpack_format)[0] }
+        let(:another_digest_num) { another_digest.reverse.unpack('H*')[0].hex }
         let(:receiver_private_key) { SecureRandom.random_number(1..group.order-1) }
         let(:receiver_public_key) { group.generate_public_key receiver_private_key }
         let(:ukm) { SecureRandom.random_number(2**(size/4)..2**(size/2)-1) }
